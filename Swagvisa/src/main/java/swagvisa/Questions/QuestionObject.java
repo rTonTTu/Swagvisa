76a22afe78ab76be2bc22793e2246aaffb421a9f
Thomas Tontchev <thomas.tontchev@cs.helsinki.fi>
2014-02-04 16:27:44 +0200

    siistitty vääriä kyssäreitä

--- Swagvisa/src/main/java/swagvisa/Questions/QuestionObject.java
+++ Swagvisa/src/main/java/swagvisa/Questions/QuestionObject.java
@@ -6,6 +6,7 @@
 package swagvisa.Questions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -15,22 +16,22 @@ import java.util.Random;
 public class QuestionObject {
 
     private Random randomGenerator;
-    private String right;
+    private String capital;
     private String wrong1;
     private String wrong2;
-    private String question;
+    private String country;
     private ArrayList<String> wrongAnswersList;
 
     public QuestionObject(String right) {
-        this.right = right;
+        this.capital = right;
         this.wrong1 = "Does not compute";
         this.wrong2 = "Woot woot?";
 
     }
 
-    public QuestionObject(String question, String right) {
-        this.question = question;
-        this.right = right;
+    public QuestionObject(String country, String capital) {
+        this.country = country;
+        this.capital = capital;
     }
 
     public void setWrong1(String answer) {
@@ -42,57 +43,20 @@ public class QuestionObject {
     }
 
     public void randomizeWrongAnswers() {
-        this.wrongAnswersList.add("Stockholm");
-        this.wrongAnswersList.add("Copenhagen");
-        this.wrongAnswersList.add("Turku");
-        this.wrongAnswersList.add("Tampere");
-        this.wrongAnswersList.add("Oslo");
-        this.wrongAnswersList.add("Ougabougagagoubou");
-        this.wrongAnswersList.add("Tokyo");
-        this.wrongAnswersList.add("Moscow");
-        this.wrongAnswersList.add("Barcelona");
-        this.wrongAnswersList.add("Sofia");
-        this.wrongAnswersList.add("Skopje");
-        this.wrongAnswersList.add("Reykjavik");
-        this.wrongAnswersList.add("Tallinn");
-        this.wrongAnswersList.add("Berlin");
-        this.wrongAnswersList.add("Frankfurt");
-        this.wrongAnswersList.add("London");
-        this.wrongAnswersList.add("Munich");
-        this.wrongAnswersList.add("Düsseldorf");
-        this.wrongAnswersList.add("Paris");
-        this.wrongAnswersList.add("New York");
-        this.wrongAnswersList.add("Washington");
-        this.wrongAnswersList.add("Seattle");
-        this.wrongAnswersList.add("Denver");
-        this.wrongAnswersList.add("New Delhi");
-        this.wrongAnswersList.add("Canberra");
-        this.wrongAnswersList.add("Melbourne");
-        this.wrongAnswersList.add("Dallas");
-        this.wrongAnswersList.add("Bern");
-        this.wrongAnswersList.add("Ankara");
-        this.wrongAnswersList.add("Budapest");
-        this.wrongAnswersList.add("Cairo");
-        this.wrongAnswersList.add("Dublin");
-        this.wrongAnswersList.add("Islamabad");
-        this.wrongAnswersList.add("Jerusalem");
-        this.wrongAnswersList.add("Luxembourg");
-        this.wrongAnswersList.add("Madrid");
-        this.wrongAnswersList.add("Minsk");
-        this.wrongAnswersList.add("Taipei");
-        this.wrongAnswersList.add("Vienna");
-        this.wrongAnswersList.add("Warsaw");
-
+        Collections.addAll(this.wrongAnswersList, "Helsink", "Stockholm", "Oslo", "Djibouti", "Tallinn", "Moscow", "Berlin", "Vienna", "Warsaw", "Reykjavik",
+                "Paris", "Madrid", "Buenos aires", "Minsk", "Sofia", "Ottawa", "Beijing", "Zagreb", "Copenhagen", "Athens", "New Delhi", "Baghdad", "Jerusalem", 
+                "Kingston", "Tokyo", "Riga", "Skopje", "Bratislava");
+ 
         while (true) {
             int index = randomGenerator.nextInt(this.wrongAnswersList.size());
-            if (!this.wrongAnswersList.get(index).equals(this.right)) {
+            if (!this.wrongAnswersList.get(index).equals(this.capital)) {
                 this.wrong1 = this.wrongAnswersList.get(index);
                 break;
             }
         }
         while (true) {
             int index = randomGenerator.nextInt(this.wrongAnswersList.size());
-            if (!this.wrongAnswersList.get(index).equals(this.right) && !this.wrongAnswersList.get(index).equals(this.wrong1)) {
+            if (!this.wrongAnswersList.get(index).equals(this.capital) && !this.wrongAnswersList.get(index).equals(this.wrong1)) {
                 this.wrong2 = this.wrongAnswersList.get(index);
                 break;
             }
