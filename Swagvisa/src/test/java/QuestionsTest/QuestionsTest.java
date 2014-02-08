fb164753b98ff156a8a8bc183ca752bec6e18590
Thomas Tontchev <thomas.tontchev@cs.helsinki.fi>
2014-02-08 18:33:19 +0200

    Päivitetty pit-testit ja kirjoitettu dokumentaatiota

--- Swagvisa/src/test/java/QuestionsTest/QuestionsTest.java
+++ Swagvisa/src/test/java/QuestionsTest/QuestionsTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package QuestionsTest;
 
 import org.junit.After;
@@ -12,34 +11,56 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import swagvisa.Questions.Question;
 
 /**
  *
  * @author TonTTu
  */
 public class QuestionsTest {
-    
+
+    private Question testQuestion;
+
     public QuestionsTest() {
+        this.testQuestion = new Question("Yolo", "You only live once");
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        
-        
+
     }
-    
+
     @After
     public void tearDown() {
     }
 
+    /**
+     * Testataan konstruktorikutsun yhteydessä luodun kysymyksen lyhennelmän oikeellisuutta.
+     */
+    
+    @Test
+    public void TestForQuestionToHaveTheRightAbbreviationUponConstruction() {
+        Question test = new Question("Yolo", "You only live once");
+        String abbreviation = "Yolo";
+        assertEquals(abbreviation, test.getAbbreviation());
+    }
+    /**
+     * Testataan konstruktorikutsun yhteydessä luodun kysymyksen selityksen oikeellisuutta.
+     */
+    @Test
+    public void TestForQuestionToHaveTheRightDefinitionUponConstruction() {
+         Question test = new Question("Yolo", "You only live once");
+        String definition = "You only live once";
+        assertEquals(definition, test.getDefinition());
+    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
