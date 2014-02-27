0f646e9a58fe105fbe822ba921cc4345109a9864
rTonTTu <rTonTTu@gmail.com>
2014-02-27 16:47:46 +0200

    Aloitettu 'vihdoin' gui

--- Swagvisa/src/main/java/swagvisa/Logic/Game.java
+++ Swagvisa/src/main/java/swagvisa/Logic/Game.java
@@ -3,19 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package swagvisa.Logic;
+
+import swagvisa.Questions.QuestionList;
+import swagvisa.User.Player;
 
 /**
  *
  * @author TonTTu
  */
 public class Game {
-    
-    
-    
-    public Game() {
+
+    private int maxQuestions;
+    private Player player;
+    private String rightAnswer;
+    private String answer;
+
+    public Game(String playerName, String playerTeam, int maxQuestions) {
+        this.maxQuestions = maxQuestions;
+        this.player = new Player(playerName, playerTeam);
         
     }
-    
+
+    public void Ask() {
+        QuestionList questions = new QuestionList();
+        this.rightAnswer = questions.getQuestionOne().getAbbreviation();
+        
+        
+        
+        if (this.answer.equals(this.rightAnswer)) {
+            this.player.answeredCorrectly();
+
+        } else {
+            this.player.answeredIncorrectly();
+        }
+
+    }
 }
--- Swagvisa/src/main/java/swagvisa/Questions/QuestionList.java
+++ Swagvisa/src/main/java/swagvisa/Questions/QuestionList.java
@@ -38,6 +38,7 @@ public class QuestionList {
         this.questionTwo = null;
         this.questionThree = null;
         addQuestions();
+        QuestionRandomizer();;
     }
 
     /**
